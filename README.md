eb5a94ed367382d2845c2c670e6864296d9a5a42
WutthinanK <developer1@npappdesign.com>
2025-09-18 15:04:10 +0700

    fix: update API base URL to production and comment out local URL

--- README.md
+++ README.md
@@ -19,3 +19,5 @@ samples, guidance on mobile development, and a full API reference.
 
 
 #fvm dart run build_runner build
+
+#fvm flutter clean && fvm dart run build_runner build --delete-conflicting-outputs && fvm flutter build apk --release 
--- lib/constants/apis.dart
+++ lib/constants/apis.dart
@@ -1,4 +1,4 @@
 class Apis {
-  static const String baseUrl = 'http://192.168.1.150:4000'; //local
-  //static const String baseUrl = 'https://api-todo.farm24dec-dev.com';
+  //static const String baseUrl = 'http://192.168.1.150:4000'; //local
+  static const String baseUrl = 'https://api-todo.farm24dec-dev.com';
 }
